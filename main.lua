6b125c22d935b1aebabed3855abf781d0fb42584
Karol Pulka <pulka.karol7@gmail.com>
2022-09-10 08:33:53 +0200

    small changes

--- main.lua
+++ main.lua
@@ -1,4 +1,5 @@
 function love.load()
+    kwadrat = love.graphics.newImage('graphics/kwadrat.png')
     sWidth,sHeight = love.graphics.getDimensions()
 end
 
@@ -7,6 +8,12 @@ function love.update(dt)
 end
 
 function love.draw()
+    love.graphics.draw(kwadrat,sWidth/2-kwadrat:getWidth()/2,sHeight/2-kwadrat:getWidth()/2)
     
 end
 
+function love.keypressed(k)
+    if k == 'escape' then
+        love.event.quit()
+    end
+end
