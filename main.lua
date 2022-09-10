2f976ad8477be3ace8f508141faa6f39033a02d2
Karol Pulka <pulka.karol7@gmail.com>
2022-09-10 08:27:06 +0200

    getting screen dimensions

--- main.lua
+++ main.lua
@@ -1,9 +1,9 @@
 function love.load()
-    
+    sWidth,sHeight = love.graphics.getDimensions()
 end
 
 function love.update(dt)
-    
+    sWidth,sHeight = love.graphics.getDimensions()
 end
 
 function love.draw()
