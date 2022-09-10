1e4c217defa4a44a9f675aa0ff2673314329df16
Karol Pulka <pulka.karol7@gmail.com>
2022-09-10 16:26:59 +0200

    drawing color squares

--- main.lua
+++ main.lua
@@ -1,6 +1,10 @@
 function love.load()
     kwadrat = love.graphics.newImage('graphics/kwadrat.png')
     sWidth,sHeight = love.graphics.getDimensions()
+    kolory = {love.graphics.newImage('graphics/kolor_czerwony.png'),
+    love.graphics.newImage('graphics/kolor_niebieski.png'),
+    love.graphics.newImage('graphics/kolor_zielony.png'),
+    love.graphics.newImage('graphics/kolor_zolty.png')}
 end
 
 function love.update(dt)
@@ -9,6 +13,14 @@ end
 
 function love.draw()
     love.graphics.draw(kwadrat,sWidth/2-kwadrat:getWidth()/2,sHeight/2-kwadrat:getWidth()/2)
+    for i=1,4 do
+        if i < 3 then
+            love.graphics.draw(kolory[i],sWidth/20,sHeight/2-kolory[i]:getHeight()*i)
+        end
+        if i > 2 then
+            love.graphics.draw(kolory[i],sWidth/20,sHeight/2+kolory[1]:getHeight()*(i-3))
+        end
+    end
     
 end
 
@@ -16,4 +28,8 @@ function love.keypressed(k)
     if k == 'escape' then
         love.event.quit()
     end
+end
+
+function love.mousepressed(b,x,y)
+
 end
